0122fdc17942014e029180631466dd7ed2e0f072
Genki Aikawa <ga07600@uga.edu>
2023-02-03 23:06:53 -0500

    QR code background color change to white

--- Profile.tsx
+++ Profile.tsx
@@ -24,11 +24,21 @@ export const Profile: FC<{}> = ({}): ReactElement => {
             <Text style={{ fontSize: 36, color: "white" }}>
               You have {points} points!
             </Text>
-            <View style={{ alignItems: "center", marginTop: 10 }}>
-              <Text style={{ marginBottom: 15, fontSize: 18, color: "white" }}>
+            <View
+              style={{
+                alignItems: "center",
+                marginTop: 10,
+                marginBottom: 10,
+                paddingBottom: 10,
+                backgroundColor: "white",
+              }}
+            >
+              <Text style={{ marginBottom: 15, fontSize: 18, color: "black" }}>
                 Use this QR code to check-in to events! 🐶
               </Text>
-              <QRCode size={200} value={userInfo.uid} />
+              <View style={{ backgroundColor: "white" }}>
+                <QRCode size={250} value={userInfo.uid} />
+              </View>
             </View>
           </>
         ) : null}
