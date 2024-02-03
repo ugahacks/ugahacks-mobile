a1a88a02a547599ba6e0be2c4e75382c5ba6f8c1
alexteal <29980694+alexteal@users.noreply.github.com>
2024-02-03 10:06:13 -0500

    remove unused code

--- app/(tabs)/Schedule.tsx
+++ app/(tabs)/Schedule.tsx
@@ -1,7 +1,4 @@
-import { ScrollView, StyleSheet, TouchableOpacity } from "react-native";
-import { Text, View, useThemeColor } from "../../components/Themed";
-import { useState } from "react";
-import Event from "../../components/Event";
+import { StyleSheet } from "react-native";
 import DaySchedule from "../../components/DaySchedule";
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
@@ -9,19 +6,6 @@ import { NavigationContainer } from "@react-navigation/native";
 
 export default function Schedule() {
   const Tab = createMaterialTopTabNavigator();
-
-  const tintColor = useThemeColor({}, "tabIconDefault");
-  const [day, setDay] = useState("Friday");
-
-  const showFridaySchedule = () => {
-    setDay("Friday");
-  };
-  const showSaturdaySchedule = () => {
-    setDay("Saturday");
-  };
-  const showSundaySchedule = () => {
-    setDay("Sunday");
-  };
 
   return (
     <NavigationContainer independent={true}>
@@ -33,57 +17,3 @@ export default function Schedule() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  dayContainer: {
-    width: "85%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 40,
-  },
-  dayButton: {
-    backgroundColor: "#C2C2C2",
-    borderRadius: 10,
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-  },
-  dayText: {
-    color: "white",
-    fontFamily: "SpaceMono",
-  },
-  selectedDay: {
-    backgroundColor: "#4478FD",
-  },
-  scheduleContainer: {
-    marginTop: 40,
-    width: "100%",
-    alignItems: "flex-start",
-  },
-  sidebar: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 0,
-    width: "100%",
-  },
-  dot: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    marginRight: 10,
-    marginLeft: 14.75,
-  },
-  verticalLine: {
-    position: "absolute",
-    height: "100%",
-    width: 1,
-    marginLeft: 19,
-  },
-});
