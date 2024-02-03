129d3bd0c67d0d344cf636a4106f8285e19002dc
alexteal <29980694+alexteal@users.noreply.github.com>
2024-02-03 10:03:55 -0500

    remove comments

--- app/(tabs)/Schedule.tsx
+++ app/(tabs)/Schedule.tsx
@@ -31,63 +31,6 @@ export default function Schedule() {
         <Tab.Screen name="Sunday" component={DaySchedule} />
       </Tab.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <View style={styles.dayContainer}>
-    //     <TouchableOpacity
-    //       style={[styles.dayButton, day === "Friday" && styles.selectedDay]}
-    //       onPress={showFridaySchedule}
-    //       activeOpacity={0.8}
-    //     >
-    //       <Text style={styles.dayText}>FRIDAY</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity
-    //       style={[styles.dayButton, day === "Saturday" && styles.selectedDay]}
-    //       onPress={showSaturdaySchedule}
-    //       activeOpacity={0.8}
-    //     >
-    //       <Text style={styles.dayText}>SATURDAY</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity
-    //       style={[styles.dayButton, day === "Sunday" && styles.selectedDay]}
-    //       onPress={showSundaySchedule}
-    //       activeOpacity={0.8}
-    //     >
-    //       <Text style={styles.dayText}>SUNDAY</Text>
-    //     </TouchableOpacity>
-    //   </View>
-    //   <View style={styles.scheduleContainer}>
-    //     <View style={[styles.sidebar, { borderColor: tintColor }]}>
-    //       <View style={[styles.verticalLine, { backgroundColor: tintColor }]} />
-    //       <View style={[styles.dot, { backgroundColor: tintColor }]} />
-    //       <Event
-    //         title="Event Check-In"
-    //         type="alert"
-    //         location="Miller Learning Center"
-    //         time="5:00pm - 6:30pm"
-    //       />
-    //     </View>
-    //     <View style={[styles.sidebar, { borderColor: tintColor }]}>
-    //       <View style={[styles.verticalLine, { backgroundColor: tintColor }]} />
-    //       <View style={[styles.dot, { backgroundColor: tintColor }]} />
-    //       <Event
-    //         title="Breakfast"
-    //         type="meal"
-    //         location="Miller Learning Center"
-    //         time="5:00pm - 6:30pm"
-    //       />
-    //     </View>
-    //     <View style={[styles.sidebar, { borderColor: tintColor }]}>
-    //       <View style={[styles.verticalLine, { backgroundColor: tintColor }]} />
-    //       <View style={[styles.dot, { backgroundColor: tintColor }]} />
-    //       <Event
-    //         title="Intro to Google Cloud"
-    //         type="tech talk"
-    //         location="Miller Learning Center"
-    //         time="5:00pm - 6:30pm"
-    //       />
-    //     </View>
-    //   </View>
-    // </View>
   );
 }
 
