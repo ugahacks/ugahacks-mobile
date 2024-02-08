4c83fc4763ef4b9af4d680ce77d0f6972f7bfe02
alexteal <29980694+alexteal@users.noreply.github.com>
2024-02-08 15:57:12 -0500

    qrcode fix

--- app/modal.tsx
+++ app/modal.tsx
@@ -15,25 +15,6 @@ export default function ModalScreen() {
       <View style={styles.qrContainer}>
         <QRCode value={user.uid} size={300} ecl={"H"} />
       </View>
-      <Text style={styles.tshirtSize}>{userRegInfo.shirtSize}</Text>
-      <View style={styles.statusContainer}>
-        <Text style={styles.statusText}>
-          check-in:{" "}
-          {userRegInfo.checkedIn ? (
-            <Text style={styles.yes}>true</Text>
-          ) : (
-            <Text style={styles.no}>false</Text>
-          )}
-        </Text>
-        <Text style={styles.statusText}>
-          check-out:{" "}
-          {userRegInfo.checkedIn ? (
-            <Text style={styles.yes}>true</Text>
-          ) : (
-            <Text style={styles.no}>false</Text>
-          )}
-        </Text>
-      </View>
     </View>
   );
 }
@@ -51,10 +32,6 @@ const styles = StyleSheet.create({
     marginTop: 50,
     borderRadius: 10,
     overflow: "hidden",
-  },
-  tshirtSize: {
-    fontSize: 48,
-    marginTop: 50,
   },
   statusContainer: {
     position: "absolute",
